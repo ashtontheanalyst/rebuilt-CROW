afc655b07412b2001b1b06cd39d24a5c5f30843e
ashtontheanalyst <ashtonrwsmith.co@gmail.com>
2025-09-16 07:32:24 -0500

    Much better UI, working JSON sending telem script, etc

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ sleek UI, better back end data processing and storing, and more efficient screen
 two bottom row displays (one being telemetry data and the other a mini-map), you can flip map styles
 by clicking the mini-map, there's a script button that demos two planes JSON files being sent and processed,
 and more.
+- TASK: Get Script 2 Running with 4 planes matched to the clients callsigns
+- TASK: Get the graphs on the screen
+- TASK: Rework the UI a bit so that all components fit nicely
 
 ## Running the App Locally:
 - Activate virtual env. in correct directory/terminal
--- static/home.js
+++ static/home.js
@@ -1,24 +1,24 @@
-// CONST: Initialize the map itself
-let mapLat;         // These are the initial map center positions
+// CONST: Initialize the map itself with init positions, object, name, type, and tile holders
+let mapLat;
 let mapLong;
 var map;
 let mapName;
-let mapType = 2;
+let mapType = 1;
 const mapBtn = document.getElementById("ctrlBtn1");
 let map1;
 let map2;
-let selectedPlaneID;
 
-// CONST: For the mini map, similar to big map
+// CONST: For the mini map
+// mapMini is the object, mini1 and 2 hold the tiles
+// maptype is always opposite of the main map so you get both styles on screen
 var mapMini;
-// It's map type is always the opposite of the regular map, so def is 1
 let mapMini1;
 let mapMini2;
-let bcdcMini;
 
 // CONST: Markers, Plane position, Icons, lines, etc. These are things ON the map
 var markers = [];
 let lastPoint = 0;
+
 // DUSK21
 var markers21 = [];
 var planePos21 = [];
@@ -30,54 +30,37 @@ var planePos17 = [];
 var planePosMini17 = [];
 var polylines17 = [];
 
-// CONST: Icons
-var mapPoint = L.icon({
-    iconUrl: './static/assets/redPointIcon.svg',
-    iconSize: [5, 5],
-    iconAnchor: [2.5, 2.5]
-});
-var blackPlaneIcon = L.icon({
-    iconUrl: './static/assets/blackPlane.svg',
+// CONST: Plane and Marker Icons
+let mapPath = "#ffd700"
+var goldPlaneIcon = L.icon({
+    iconUrl: './static/assets/goldPlaneIcon.svg',
     iconSize: [40, 40],
     iconAnchor: [20, 20]
 });
-var whitePlaneIcon = L.icon({
-    iconUrl: './static/assets/whitePlane.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 20]
+var goldPlaneIconMini = L.icon({
+    iconUrl: './static/assets/goldPlaneIcon.svg',
+    iconSize: [30, 30],
+    iconAnchor: [15, 15]
 });
-var blackPlaneIconMini = L.icon({
-    iconUrl: './static/assets/blackPlane.svg',
-    iconSize: [15, 15],
-    iconAnchor: [7.5, 7.5]
+var goldMarker = L.icon({
+    iconUrl: './static/assets/goldMarker.svg',
+    iconSize: [1, 1],
+    iconAnchor: [0.5, 0.5]
 });
-var whitePlaneIconMini = L.icon({
-    iconUrl: './static/assets/whitePlane.svg',
-    iconSize: [15, 15],
-    iconAnchor: [7.5, 7.5]
-});
+
+// CONST: BCDC Icons
 let bcdc;
-var blackBCDCIcon = L.icon({
-    iconUrl: './static/assets/blackBcdcIcon.svg',
+let bcdcMini;
+var bcdcIcon = L.icon({
+    iconUrl: './static/assets/bcdcIcon.svg',
     iconSize: [50, 50],
     iconAnchor: [25, 25]
 });
-var whiteBCDCIcon = L.icon({
-    iconUrl: './static/assets/whiteBcdcIcon.svg',
-    iconSize: [50, 50],
-    iconAnchor: [25, 25]
+var bcdcIconMini = L.icon({
+    iconUrl: './static/assets/bcdcIcon.svg',
+    iconSize: [25, 25],
+    iconAnchor: [12.5, 12.5]
 });
-var blackBCDCIconMini = L.icon({
-    iconUrl: './static/assets/blackBcdcIcon.svg',
-    iconSize: [20, 20],
-    iconAnchor: [10, 10]
-});
-var whiteBCDCIconMini = L.icon({
-    iconUrl: './static/assets/whiteBcdcIcon.svg',
-    iconSize: [20, 20],
-    iconAnchor: [10, 10]
-});
-let mapPath = "#ee323eff"
 
 // CONST: Scripts
 const optnsBox = document.getElementById("optnsBox");
@@ -91,7 +74,7 @@ const script2Btn = document.getElementById("ctrlBtn2");
 
 
 // CONST: Bottom Telem Box
-let telemClicked = false;
+let trackedCallsign;
 const telemBox = document.getElementById("telemBox");
 const telemTitle = document.getElementById("telemTitle");
 
@@ -101,34 +84,35 @@ function initMap() {
     // This is THE map object, super important
     mapLat = 30.6328;
     mapLong = -96.3705;
-    map = L.map('map').setView([mapLat, mapLong], 12);
+    map = L.map('map', {zoomControl: false}).setView([mapLat, mapLong], 12).on("click", () => {
+        trackedCallsign = null;
+        clearTelemBox();
+    });
     mapName = document.createElement("p");
 
     // Adding a tile layer, we have different options right now based on the mapType value
     if (mapType == 1) {
-        map1 = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        map1 = L.tileLayer('http://{s}.google.com/vt?lyrs=p&x={x}&y={y}&z={z}',{
+            maxZoom: 20,
+            subdomains:['mt0','mt1','mt2','mt3'],
+            opacity: 0.75
         }).addTo(map);
-
-        // Add BCDC icon
-        bcdc = L.marker([30.6376, -96.4891], {icon: blackBCDCIcon}).addTo(map)
-            .bindPopup("BCDC");
 
         mapName.textContent = "Map Type: Style 1"
         optnsBox.appendChild(mapName);
-    } else if (mapType == 2) {
+    } 
+    else if (mapType == 2) {
         map2 = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
-			maxZoom: 20,
-			attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+			maxZoom: 20
 		}).addTo(map);
-
-        bcdc = L.marker([30.6376, -96.4891], {icon: whiteBCDCIcon}).addTo(map)
-            .bindPopup("BCDC");
 
         mapName.textContent = "Map Type: Style 2"
         optnsBox.appendChild(mapName);
     }
+
+    // BCDC Icon
+    bcdc = L.marker([30.6376, -96.4891], {icon: bcdcIcon}).addTo(map)
+            .bindPopup("BCDC");
 }
 
 
@@ -140,25 +124,23 @@ function initMapMini() {
         doubleClickZoom: false,
         dragging: false,
         scrollWheelZoom: false
-    }).setView([mapLat, mapLong], 10);
+    }).setView([mapLat, mapLong], 9);
 
     if (mapType == 1) {
         mapMini2 = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
-			maxZoom: 20,
-			attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+			maxZoom: 20
 		}).addTo(mapMini);
-
-        bcdcMini = L.marker([30.6376, -96.4891], {icon: whiteBCDCIconMini}).addTo(mapMini)
-            .bindPopup("BCDC");
-    } else if (mapType == 2) {
-        mapMini1 = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    } 
+    else if (mapType == 2) {
+        mapMini1 = googleStreets = L.tileLayer('http://{s}.google.com/vt?lyrs=p&x={x}&y={y}&z={z}',{
+            maxZoom: 20,
+            subdomains:['mt0','mt1','mt2','mt3'],
+            opacity: 0.75
         }).addTo(mapMini);
-
-        bcdcMini = L.marker([30.6376, -96.4891], {icon: blackBCDCIconMini}).addTo(mapMini)
-            .bindPopup("BCDC");
     }
+
+    bcdcMini = L.marker([30.6376, -96.4891], {icon: bcdcIconMini}).addTo(mapMini)
+            .bindPopup("BCDC");
 }
 
 
@@ -167,54 +149,43 @@ function changeMap() {
     if (mapType == 1) {
         // For the big map
         map.removeLayer(map1);
-        map.removeLayer(bcdc);
         
         mapName.textContent = "Map Type: Style 2";
         
         map2 = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
-			maxZoom: 20,
-			attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+			maxZoom: 20
 		}).addTo(map);
-        bcdc = L.marker([30.6376, -96.4891], {icon: whiteBCDCIcon}).addTo(map)
-            .bindPopup("BCDC");
 
         // For the mini map
         if (mapMini1) mapMini.removeLayer(mapMini1);
         if (mapMini2) mapMini.removeLayer(mapMini2);
-        if (bcdcMini) mapMini.removeLayer(bcdcMini);
 
-        mapMini1 = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        mapMini1 = googleStreets = L.tileLayer('http://{s}.google.com/vt?lyrs=p&x={x}&y={y}&z={z}',{
+            maxZoom: 20,
+            subdomains:['mt0','mt1','mt2','mt3'],
+            opacity: 0.75
         }).addTo(mapMini);
-        bcdcMini = L.marker([30.6376, -96.4891], {icon: blackBCDCIconMini}).addTo(mapMini)
-            .bindPopup("BCDC");
 
         mapType = 2;
-    } else if (mapType == 2) {
+    } 
+    else if (mapType == 2) {
         map.removeLayer(map2);
-        map.removeLayer(bcdc);
         
         mapName.textContent = "Map Type: Style 1"
 
-        map1 = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        map1 = L.tileLayer('http://{s}.google.com/vt?lyrs=p&x={x}&y={y}&z={z}',{
+            maxZoom: 20,
+            subdomains:['mt0','mt1','mt2','mt3'],
+            opacity: 0.75
         }).addTo(map);
-        bcdc = L.marker([30.6376, -96.4891], {icon: blackBCDCIcon}).addTo(map)
-            .bindPopup("BCDC");
 
         // For the mini Map
         if (mapMini1) mapMini.removeLayer(mapMini1);
         if (mapMini2) mapMini.removeLayer(mapMini2);
-        if (bcdcMini) mapMini.removeLayer(bcdcMini);
 
         mapMini2 = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
-			maxZoom: 20,
-			attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+			maxZoom: 20
 		}).addTo(mapMini);
-        bcdcMini = L.marker([30.6376, -96.4891], {icon: whiteBCDCIconMini}).addTo(mapMini)
-            .bindPopup("BCDC");
 
         mapType = 1;
     }
@@ -234,8 +205,9 @@ async function updateMap() {
         // Only add a point if we don't already have it on the map/in the marker array at large
         if (!markers.some(([id]) => id === point.id)) {
  
-            // Add the marker on the map and add to the array with it's identifier
-            const marker = L.marker([point.lat, point.long], {icon: mapPoint}).addTo(map);
+            // Add the plane positional markers on the screen
+            const marker = L.marker([point.lat, point.long], {icon: goldMarker}).addTo(map);
+            // Save the marker icons of the planes to the associated array
             switch (point.callsign) {
                 case "DUSKY21":
                     markers21.push([point.id, marker]);
@@ -264,33 +236,41 @@ async function updateMap() {
                     break;
             }
 
-            // This is making the plane show up, the color of the plane changes based on the background map style
+            // This all is what makes the plane icons show up on big and mini map
             const heading = point.heading;
             let plane;
             let planeMini;
             if (mapType == 1) {
                 plane = L.marker([point.lat, point.long], {
-                    icon: blackPlaneIcon,
+                    icon: goldPlaneIcon,
                     // This is our GitHub addition for rotating the plane based on heading
                     rotationAngle: heading,
                     rotationOrigin: 'center center'
-                }).addTo(map).on("click", () => {telemClicked = true});
+                }).addTo(map).on("click", () => {
+                    // If the plane is clicked, it'll live update the telembox with it's associated info
+                    trackedCallsign = point.callsign;
+                    updateTelemBox(point);
+                });
 
                 // This adds it to the mini map
                 planeMini = L.marker([point.lat, point.long], {
-                    icon: whitePlaneIconMini,
+                    icon: goldPlaneIconMini,
                     rotationAngle: heading,
                     rotationOrigin: 'center center'
                 }).addTo(mapMini);
-            } else if (mapType == 2) {
+            } 
+            else if (mapType == 2) {
                 plane = L.marker([point.lat, point.long], {
-                    icon: whitePlaneIcon,
+                    icon: goldPlaneIcon,
                     rotationAngle: heading,
                     rotationOrigin: 'center center'
-                }).addTo(map).on("click", () => {telemClicked = true});
+                }).addTo(map).on("click", () => {
+                    trackedCallsign = point.callsign;
+                    updateTelemBox(point);
+                });
 
                 planeMini = L.marker([point.lat, point.long], {
-                    icon: blackPlaneIconMini,
+                    icon: goldPlaneIconMini,
                     rotationAngle: heading,
                     rotationOrigin: 'center center'
                 }).addTo(mapMini);
@@ -345,38 +325,70 @@ async function updateMap() {
         // Basically, increment up from the last ID
         lastPoint = Math.max(lastPoint, point.id);
 
-        // Update the telembox with better data if we have a click on a plane
-        if (telemClicked == true) {
+        // This is what keeps the telembox updating every second if it's been selected (plane's been clicked)
+        if (trackedCallsign && point.callsign === trackedCallsign) {
             updateTelemBox(point);
         }
     });
+
+    // Keep getLabelInfo up to date
+    getLabelInfo(data);
 }
 
 
-// When a plane is clicked, we keep updating the telemetry box in the bottom row as it comes in
+// This is what updates the telembox. We clear it's data then replace with fresh
 function updateTelemBox(point) {
-    // delete old data so we can replace it
     telemBox.innerHTML = "";
+    telemTitle.textContent = `${point.callsign}`;
 
-    // Replace the heading with the plane's ID
-    telemTitle.textContent = `Callsign: ${point.callsign}`;
-
-    // Create our data based on input thrown to this function (most recent point)
+    // Create our elements and fill them
     let telemLat = document.createElement("p");
     telemLat.id = "telemLat";
     telemLat.textContent = `LAT: ${Number(point.lat).toFixed(4)}`;
 
     let telemLong = document.createElement("p");
     telemLong.id = "telemLong";
-    telemLong.textContent = `LON: ${Number(point.long).toFixed(4)}`;
-
+    telemLong.textContent = `LON: ${Number(point.long).toFixed(4)}`;  
+    
     let telemHeading = document.createElement("p");
     telemHeading.id = "telemHeading";
     telemHeading.textContent = `DIR: ${Number(point.heading)}`;
 
     telemBox.append(telemLat, telemLong, telemHeading);
+}
 
-    selectedPlaneID = point.id;
+// This is mainly to reset the box to default when a user clicks the MAP and not a PLANE
+function clearTelemBox() {
+    telemTitle.textContent = "Select a Plane";
+    telemBox.innerHTML = "";
+}
+
+
+// This is what will display the info when the button is toggled next to the plane (bottom right)
+function toggleLabels() {
+    if (markers.length > 0) {
+        return;
+    }
+}
+
+
+// Toggle labels is fired when the button is clicked, shows flight data by the plane on map
+function getLabelInfo(data) {
+    data.forEach((point) => {
+        console.log(point);
+
+        // Separate the data to specific planes
+        switch (point.callsign) {
+            case "DUSKY17":
+                return;
+                break;
+            case "DUSKY17":
+                return;
+                break;
+            default:
+                console.log("NOTE: getLabelData() not receiving pre-determined named drone data");
+        }
+    });
 }
 
 
@@ -495,69 +507,3 @@ map.on("move", () => {
 
 updateMap();
 setInterval(updateMap, 250);
-
-
-
-
-/* OLD SCRIPT
-// This runs a script for one plane on the screen based on clicking the button on/off
-let script1Interval = null;         // This will help the script STOP when we re-click
-
-function script1() {
-    // If the toggle button is on 1, 'not running', set it to 2, 'running'
-    if (script1Toggle == 1) {
-        script1Toggle = 2;
-
-        script1LabelMake(2);
-        
-        // Start the script, run it every second to the Flask side until we click the button again
-        script1Interval = setInterval(async () => {
-            await fetch("/script1", {method: "POST"});
-        }, 1000);
-    } else if (script1Toggle == 2) {
-        // Paused state, we can view the plane's points, path, etc.
-        script1Toggle = 3;
-
-        script1LabelMake(3);
-
-        clearInterval(script1Interval);
-        script1Interval = null;
-    }
-    else if (script1Toggle == 3) {
-        // If the script is paused and we click again, reset everything, set back to 1 which is 'not running'
-        script1Toggle = 1;
-        clearInterval(script1Interval);
-        script1Interval = null;
-
-        // For this case, it just removes the label
-        script1LabelMake(1);
-
-        clearMap();
-        fetch("/script1/reset", {method: "POST"});
-    }
-}
-
-// This creates the label that will be put in the monitor box in top right
-function script1LabelMake(state) {
-    // delete the old label
-    if (script1Label) {
-        script1Label.remove();
-    }
-
-    // Remake it, or for case 3 do nothing since we want it to stay nothing
-    script1Label = document.createElement("p");
-    script1Label.id = "script1Label";
-    switch (state) {
-        case 2:
-            script1Label.textContent = "Script 1: Running";
-            optnsBox.appendChild(script1Label);
-            break;
-        case 3:
-            script1Label.textContent = "Script 1: Paused";
-            optnsBox.appendChild(script1Label);
-            break;
-        case 1:
-            break;
-    }
-} 
-*/
